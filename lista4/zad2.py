79409db84d84906680533f1c6c89ba137ec6a322
SonOfGrabarz <59028387+SonOfGrabarz@users.noreply.github.com>
2020-11-18 14:44:29 +0100

    Add files via upload

--- lista4/zad2.py
+++ lista4/zad2.py
@@ -1,8 +1,14 @@
 from scipy.optimize import newton
 import numpy as np
 
-f = lambda x, a: x**5 - a
-df = lambda x, a: 5*x**4
-a = np.arange(0, 10)
-x = np.random.randn(10)
-print(newton(f, x, fprime=df, args=(a,)))
+
+def f(a):
+    f = lambda x, a: x**5 - a
+    df = lambda x, a: 5*x**4
+    x = np.random.randn(1)
+    print(newton(f, x, fprime=df, args=(a,)))
+
+
+f(4)
+f(10)
+f(21)
