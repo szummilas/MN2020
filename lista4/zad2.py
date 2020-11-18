4bd5f452be26a41228dc345085e25b8e756cf373
SonOfGrabarz <59028387+SonOfGrabarz@users.noreply.github.com>
2020-11-18 14:33:10 +0100

    Update zad2.py

--- lista4/zad2.py
+++ lista4/zad2.py
@@ -1,9 +1,8 @@
 from scipy.optimize import newton
-from numpy import arange
-from numpy import random
+import numpy as np
 
 f = lambda x, a: x**5 - a
 df = lambda x, a: 5*x**4
-a = arange(0, 10)
-x = random.randn(10)
+a = np.arange(0, 10)
+x = np.random.randn(10)
 print(newton(f, x, fprime=df, args=(a,)))
